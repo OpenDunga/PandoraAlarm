1662ea7c1d27e7ab851645cb8daabc80bda3f9a7
giginet <giginet.net@gmail.com>
2012-05-12 14:10:35 +0900

    Recordモデル実装

---
+++ DungaAlerm/Model/DARecord.h
@@ -0,0 +1,17 @@
+//
+//  DARecord.h
+//  DungaAlerm
+//
+//  Created by  on 2012/5/12.
+//  Copyright (c) 2012 Kawaz. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DARecord : NSObject
+
+@property(readwrite, strong) NSString* label;
+@property(readwrite, strong) NSString* message;
+@property(readwrite, strong) NSData* rawAudio;
+
+@end
---
+++ DungaAlerm/Model/DARecord.m
@@ -0,0 +1,26 @@
+//
+//  DARecord.m
+//  DungaAlerm
+//
+//  Created by  on 2012/5/12.
+//  Copyright (c) 2012 Kawaz. All rights reserved.
+//
+
+#import "DARecord.h"
+
+@implementation DARecord
+@synthesize label;
+@synthesize message;
+@synthesize rawAudio;
+
+- (id)init {
+  self = [super init];
+  if (self) {
+    self.label = @"";
+    self.message = @"";
+    self.rawAudio = [NSData data];
+  }
+  return self;
+}
+
+@end
